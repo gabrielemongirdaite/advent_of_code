b4878b006657e71696f5dc61519ddb7346b82966
gabrielemongirdaite <58396325+gabrielemongirdaite@users.noreply.github.com>
2019-12-19 23:10:10 +0200

    Add files via upload
    Day 13 of advent of code

--- intcode.py
+++ intcode.py
@@ -34,8 +34,8 @@ def TEST2(l, test_input, starting_point, relative_base):
     i=starting_point
     result = []
     relative_base = relative_base
-    while get_digit(l[i],0)!=99:
-        opcode = get_digit(l[i],0)
+    while (get_digit(l[i],0)+get_digit(l[i],1)*10)!=99:
+        opcode = (get_digit(l[i],0)+get_digit(l[i],1)*10)
         mode1 = get_digit(l[i],2)
         mode2 = get_digit(l[i],3)
         mode3 = get_digit(l[i],4)
@@ -51,9 +51,63 @@ def TEST2(l, test_input, starting_point, relative_base):
             l = l_update(l, i+1, relative_base, mode1, test_input)
             i = i+2
         elif opcode == 4:
+            #print(n1, i)
             result.append(n1)
             i = i+2
-            if len(result)==2:
+            #if len(result)==2:
+            #   return result, l, i, relative_base
+        elif opcode == 5:
+            if n1 != 0:
+                i = n2
+            else:
+                i = i+3
+        elif opcode == 6:
+            if n1 == 0:
+                i = n2
+            else:
+                i = i+3
+        elif opcode == 7:
+            if n1<n2:
+                l = l_update(l, i+3, relative_base, mode3, 1)
+            else:
+                l = l_update(l, i+3, relative_base, mode3, 0)
+            i = i+4
+        elif opcode == 8:
+            if n1==n2:
+                l = l_update(l, i+3, relative_base, mode3, 1)
+            else:
+                l = l_update(l, i+3, relative_base, mode3, 0)
+            i = i+4
+        elif opcode == 9:
+            relative_base = relative_base+n1
+            i = i+2  
+    return result
+
+def TEST3(l, test_input, starting_point, relative_base):
+    i=starting_point
+    result = []
+    relative_base = relative_base
+    while (get_digit(l[i],0)+get_digit(l[i],1)*10)!=99:
+        opcode = (get_digit(l[i],0)+get_digit(l[i],1)*10)
+        mode1 = get_digit(l[i],2)
+        mode2 = get_digit(l[i],3)
+        mode3 = get_digit(l[i],4)
+        n1 = mode(l, i+1, mode1, relative_base)
+        n2 = mode(l, i+2, mode2, relative_base)
+        if opcode == 1:
+            l = l_update(l, i+3, relative_base, mode3, n1+n2)
+            i = i+4
+        elif opcode == 2:
+            l = l_update(l, i+3, relative_base, mode3, n1*n2)
+            i = i+4
+        elif opcode == 3:
+            l = l_update(l, i+1, relative_base, mode1, test_input)
+            i = i+2
+        elif opcode == 4:
+            #print(n1, i)
+            result.append(n1)
+            i = i+2
+            if len(result)==3:
                 return result, l, i, relative_base
         elif opcode == 5:
             if n1 != 0:
@@ -79,6 +133,5 @@ def TEST2(l, test_input, starting_point, relative_base):
             i = i+4
         elif opcode == 9:
             relative_base = relative_base+n1
-            i = i+2
-        else:
-            return 
+            i = i+2  
+    return 
